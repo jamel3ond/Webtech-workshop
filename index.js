825cd3a490617ae6a1acc691ddaa7a8bb6427d1d
jamel3ond <31301098+jamel3ond@users.noreply.github.com>
2017-11-28 21:13:13 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-
-router.get('/Contacts', (req, res) =>{
-    res.json({id : 1 , name: 'Thanapat'})
-})
-
-module.exports = router
